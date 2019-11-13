c9b6a4d559362a451b6de618d3fa4c7974930e57
Phonekham <phone.madridista@gmail.com>
2019-11-13 09:04:52 +0700

    add font

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/OpenSans-Bold.ttf")
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setFontLoaded(true)}
+    ></AppLoading>;
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -12,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
