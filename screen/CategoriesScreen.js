134f6389caeb71bc53d084eb26bb503e7aae6366
Phonekham <phone.madridista@gmail.com>
2019-11-13 10:56:39 +0700

    Outputting a Grid of Categories

--- screen/CategoriesScreen.js
+++ screen/CategoriesScreen.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 
+import { CATEGORIES } from "../data/dummy-data";
+
+const renderGridItem = itemData => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
+
 const CategoriesScreen = props => {
-  return <FlatList numColumns={2}></FlatList>;
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    ></FlatList>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -10,6 +27,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 });
 
