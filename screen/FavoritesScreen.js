fe06d0e8aa82349d0ba507fab1627821ff486995
Phonekham <phone.madridista@gmail.com>
2019-11-20 11:35:41 +0700

    Selecting State Slices

--- screen/FavoritesScreen.js
+++ screen/FavoritesScreen.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { useSelector } from "react-redux";
 
 import MealList from "../components/MealList";
-import { MEALS } from "../data/dummy-data";
 import HeaderButton from "../components/HeaderButton";
 
 const FavoritesScreen = props => {
-  const favMeal = MEALS.filter(meal => meal.id === "m1" || meal.id === "m2");
-  return <MealList listData={favMeal} navigation={props.navigation}></MealList>;
+  const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  return (
+    <MealList listData={favMeals} navigation={props.navigation}></MealList>
+  );
 };
 
 FavoritesScreen.navigationOptions = navDate => {
