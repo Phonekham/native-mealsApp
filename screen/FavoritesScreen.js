b095a337e5a542c40924f250950a39978047df14
Phonekham <phone.madridista@gmail.com>
2019-11-23 09:44:58 +0700

    Rendering a Fallback Text if no favorite

--- screen/FavoritesScreen.js
+++ screen/FavoritesScreen.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { useSelector } from "react-redux";
 
 import MealList from "../components/MealList";
 import HeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/DefaultText";
 
 const FavoritesScreen = props => {
   const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favorite found</DefaultText>
+      </View>
+    );
+  }
 
   return (
     <MealList listData={favMeals} navigation={props.navigation}></MealList>
@@ -29,5 +39,13 @@ FavoritesScreen.navigationOptions = navDate => {
     )
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default FavoritesScreen;
